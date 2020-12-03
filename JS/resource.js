38878d05017630e3547bd0406c1eb50a8b52143d
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2020-12-03 23:40:27 +0530

    Create resource.js

---
+++ JS/resource.js
@@ -0,0 +1,14 @@
+var blue = new Audio('sounds/blue.mp3');
+blue.load();
+
+var green = new Audio('sounds/green.mp3');
+green.load();
+
+var red = new Audio('sounds/red.mp3');
+red.load();
+
+var yellow = new Audio('sounds/yellow.mp3');
+yellow.load();
+
+var wrong = new Audio('sounds/wrong.mp3');
+wrong.load();
