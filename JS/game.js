994d7bbfcf0363e77f3d1c27ba33a4403f56d7ec
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2020-12-03 20:50:51 +0530

    Start game and check for users answer

--- JS/game.js
+++ JS/game.js
@@ -2,15 +2,29 @@ var buttonColours = ["red", "green", "blue", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
 
+var level = 0;
+var started = false;
+
 $(".btn").click(function () {
     var userChosenColour = $(this).attr("id");
     userClickedPattern.push(userChosenColour);
     
     playSound(userChosenColour);
     animatePress(userChosenColour);
+
+    checkSequence(userClickedPattern.length-1);
+});
+
+$(document).keydown(function () {
+    if (!started) {
+        started = true;
+        nextSequence();
+    }
 });
 
 function nextSequence () {
+    userClickedPattern = [];
+    
     var randomNumber = Math.floor(Math.random() * 4);
     var randomColourChooser = buttonColours[randomNumber];
     gamePattern.push(randomColourChooser);
@@ -18,6 +32,21 @@ function nextSequence () {
     $("#" + randomColourChooser).fadeOut(150).fadeIn(150);
 
     playSound(randomColourChooser);
+
+    level++;
+    $("#level-title").text("Level " + level);
+}
+
+function checkSequence (idx) {
+    if (gamePattern[idx] == userClickedPattern[idx]) {
+        console.log("success");
+        setTimeout(function () {
+            nextSequence();
+        }, 1000);
+    }
+    else {
+        console.log("wrong");
+    }
 }
 
 function playSound (name) {
@@ -32,4 +61,4 @@ function animatePress (currentColour) {
     }, 80);
 }
 
-nextSequence();
+// nextSequence();
